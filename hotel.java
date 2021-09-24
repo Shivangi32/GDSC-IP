b9e687d699bb4181199dfc20585ae2b66b42fd85
Shivangi32 <sdatisha.2195@gmail.com>
2021-09-24 17:34:35 +0530

    commit

---
+++ hotel.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+
+    public class Pair implements Comparable<Pair>{
+
+        int start;
+        int end;
+        Pair(int s,int e)
+        {
+            start=s;
+            end=e;
+        }
+        public int compareTo(Pair p)
+        {
+            if(this.start<p.start)
+              return -1;
+            if(this.start>p.start)
+              return 1;
+            return 0;
+        }
+    }
+    public boolean hotel(ArrayList<Integer> arrive, ArrayList<Integer> depart, int c) {
+
+        int i=0,j=0;
+        int count=0;
+        Pair[] p=new Pair[arrive.size()];
+        for (int k = 0; k < arrive.size(); k++) {
+           p[i]=new Pair(arrive.get(k),depart.get(k));
+        }
+        Arrays.sort(p);
+        while(i<p.length && j<p.length)
+        {
+            if(p[i].start<p[j].end)
+            {
+                count++;
+                i++;
+            }
+            else
+            {
+                count--;
+                j++;
+            }
+            if(count>c)
+              return false;
+        }
+        return true;
+    }
+}

8a0b553eb1cb1bcbbd22411c5c57c4472e54b040
Shivangi32 <sdatisha.2195@gmail.com>
2021-09-28 22:03:46 +0530

    commit

---
+++ Day 5/search_sorted _array_II.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        
+        int lo=0,hi=nums.length-1;
+        while(lo<=hi)
+        {
+            int mid=(lo+hi)/2;
+            if(target==nums[mid])
+                return true;
+            else if(nums[lo]==nums[mid] && nums[mid]==nums[hi])
+            {
+                lo++;
+                hi--;
+            }
+            else if(nums[lo]<=nums[mid])
+            {
+                if(target>=nums[lo] && target<nums[mid])
+                {
+                    hi=mid-1;
+                }
+                else
+                    lo=mid+1;
+            }
+            else if(nums[mid]<=nums[hi])
+            {
+                if(target>nums[mid] && target<=nums[hi])
+                {
+                    lo=mid+1;
+                }
+                else
+                    hi=mid-1;
+            }
+        }
+        return false;
+    }
+}

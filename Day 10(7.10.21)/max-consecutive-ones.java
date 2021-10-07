f2121d90e13c616a41a1f7f1645cb440ad953f47
Shivangi32 <sdatisha.2195@gmail.com>
2021-10-07 18:57:55 +0530

    commit

---
+++ Day 10(7.10.21)/max-consecutive-ones.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        
+        int cnt=0;
+        int ans=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==1)
+                cnt++;
+            else
+            {
+                ans=Math.max(ans,cnt);
+                cnt=0;
+            }
+        }
+        ans=Math.max(ans,cnt);
+        return ans;
+        
+    }
+}

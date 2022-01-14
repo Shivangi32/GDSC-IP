adb5083422ba91e3dca5eaf44f6ca4a14388488d
Shivangi32 <sdatisha.2195@gmail.com>
2022-01-14 15:14:03 +0530

    commit

--- Day(26.11.21)/coloring_A_border.java
+++ Day(26.11.21)/coloring_A_border.java
@@ -1,32 +1,36 @@
 class Solution {
     
-    public int help(int[][] grid,int i,int j,int val,int color,boolean visited[][]){
-        
-       
-        if(i<0 || j<0 || i==grid.length || j==grid[0].length)
+    public int help(int grid[][],int i,int j,int val,int color)
+      {
+          if(i<0 || j<0 || i>=grid.length || j>=grid[0].length)
             return 0;
-         if(visited[i][j]==true)
+          if(grid[i][j]==-1*val)
             return 1;
-        if(grid[i][j]!=val)
+          if(grid[i][j]!=val)
             return 0;
-        visited[i][j]=true;
-        int faith=0;
-        faith+=help(grid,i,j+1,grid[i][j],color,visited); //right
-        faith+=help(grid,i,j-1,grid[i][j],color,visited); //left
-        faith+=help(grid,i-1,j,grid[i][j],color,visited); //up
-        faith+=help(grid,i+1,j,grid[i][j],color,visited); //down
-        System.out.println(i+" "+j+": "+faith);
-        if(faith<4)
-            grid[i][j]=color;
-        return 1;
-        
-    }
-    public int[][] colorBorder(int[][] grid, int row, int col, int color) {
-        
-        int m=grid.length;
-        int n=grid[0].length;
-        boolean visited[][]=new boolean[m][n];
-        help(grid,row,col,grid[row][col],color,visited);
-        return grid;
-    }
+          grid[i][j]*=-1;
+          int faith=0;
+          faith+=help(grid,i,j+1,val,color); //right
+          faith+=help(grid,i,j-1,val,color); //left
+          faith+=help(grid,i-1,j,val,color); //up
+          faith+=help(grid,i+1,j,val,color); //down
+          
+          if(faith==4)
+            grid[i][j]*=-1;
+          return 1;
+          
+      }
+      public int[][] colorBorder(int[][] grid, int row, int col, int color) {
+          
+          help(grid,row,col,grid[row][col],color);
+          for(int i=0;i<grid.length;i++)
+          {
+              for(int j=0;j<grid[0].length;j++)
+              {
+                  if(grid[i][j]<0)
+                     grid[i][j]=color;
+              }
+          }
+          return grid;
+      }
 }
